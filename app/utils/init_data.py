6b09d678fb82f9030495f561f0110a3ae293db34
sequence <stuff161@yandex.ru>
2019-11-03 00:33:07 +0400

    fix

--- app/utils/init_data.py
+++ app/utils/init_data.py
@@ -25,18 +25,25 @@ def info_property():
         ContactProperty(
             key='map_widget_code',
             value="""
-                <a class="dg-widget-link" 
-                href="http://2gis.ru/rostov/firm/70000001007492088/center/39.685563,47.219826/zoom/16?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=bigMap">
-                Посмотреть на карте Ростова-на-Дону
-                </a>
-                <div class="dg-widget-link">
-                <a href="http://2gis.ru/rostov/center/39.685563,47.219826/zoom/16/routeTab/rsType/bus/to/39.685563,47.219826╎Piano, музыкальная школа для взрослых и детей?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=route">
-                Найти проезд до Piano, музыкальная школа для взрослых и детей
-                </a>
-                </div>
-                <script charset="utf-8" src="https://widgets.2gis.com/js/DGWidgetLoader.js"></script>
-                <script charset="utf-8">new DGWidgetLoader({"width":640,"height":600,"borderColor":"#a3a3a3","pos":{"lat":47.219826,"lon":39.685563,"zoom":16},"opt":{"city":"rostov"},"org":[{"id":"70000001007492088"}]});</script>
-                <noscript style="color:#c00;font-size:16px;font-weight:bold;">Виджет карты использует JavaScript. Включите его в настройках вашего браузера.</noscript>
+                <script src="https://widgets.2gis.com/js/DGWidgetLoader.js"></script>
+                <script>
+                    let interval = setInterval(()=>{
+                    if(window.DGWidgetLoader){
+                        new DGWidgetLoader({
+                            "width": 640,
+                            "height": 600,
+                            "borderColor": "#a3a3a3",
+                            "pos": {"lat": 47.219826, "lon": 39.685563, "zoom": 16},
+                            "opt": {"city": "rostov"},
+                            "org": [{"id": "70000001007492088"}]
+                        });
+                        clearInterval(interval);
+                    }
+                    }, 10)
+                </script>
+                <noscript style="color:#c00;font-size:16px;font-weight:bold;">Виджет карты использует JavaScript. Включите его в
+                    настройках вашего браузера.
+                </noscript>
             """,
         ),
     ])
